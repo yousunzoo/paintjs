66c2b306bc3a8886ac762e5ac68cf99f7c614cce
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-05-18 12:07:06 +0900

    color 변경

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-// canvas 요소는 두가지 사이즈를 가짐. 1) css에서 조정하는 사이즈, 2) js 작동을 위한 사이즈 context 속성을 가짐
-//context : 요소 안에서 픽셀에 접근할 수 있는 방법
-
+/**
+ * canvas 요소는 두가지 사이즈를 가짐.
+ * 1) css에서 조정하는 사이즈
+ * 2) js 작동을 위한 사이즈 context 속성을 가짐 (manipulationg)
+ * context : 요소 안에서 픽셀에 접근할 수 있는 방법. 픽셀들을 컨트롤
+*/
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName('js_color');
 
 // pixel modifier
 canvas.width = 700;
 canvas.height = 700;
 
+// 초기 브러쉬 색상과 굵기 설정
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -27,20 +32,24 @@ function startPainting(){
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
+  // 마우스를 움직일 때, 마우스의 위치에 따라 path가 만들어짐
+  // 마우스를 눌러서 painting=true 일 때 path의 시작점(x,y)부터 마우스를 누른 상태일 때(x,y)까지 직선이 만들어짐
+  // 마우스를 움직일 때마다 이벤트 발생해서 선이 이어지도록
   if(!painting){
+    // console.log("creating path in " ,x,y)
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
+    // console.log("creating line in " ,x,y)
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 };
 
-// 마우스 누르고 있을 때 브러쉬 동작하도록
-function onMouseDown(event){
-  painting = true;
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
-
 
 // 이벤트 설정
 if(canvas){
@@ -49,4 +58,4 @@ if(canvas){
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 } 
-
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
